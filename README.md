fd82e0308c4afe8fb0715c751a463a1de280c87f
yauhenibh <yauhenibh@gmail.com>
2018-09-23 16:58:00 +0300

    Adding lintero

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # ClearSkyMaps
 This project is used for building safe routes without toxic and dangerous areas!
 
-#Usefull links
+# Usefull links
 
 1. https://developers.google.com/maps/documentation/javascript/examples/circle-simple
 2. https://developers.google.com/maps/documentation/javascript/geometry#isLocationOnEdge
 3. https://developers.google.com/maps/documentation/javascript/examples/poly-containsLocation
+
+#TO DO
+
+1. Создать CalculationService который будет определять уровень угрозы по показаниям, по сенсору, за период и тд
+2. Возвращать из сервиса уровень опасност
+3. Добавить на фронт переключатель допустимого уровня опасности
+4. Отправлять на фронт вместе с показателями уровень последние рассчеты по уровню опасности и привязывать их к сенсору
+5. Подбирать сенсоры для проверки в isOnEdge на основе выбранного уровня опасности
---
+++ src/Web/MapsRouting/MapsRoutingHelper.cs
@@ -0,0 +1,44 @@
+﻿using Itinero;
+using Itinero.Osm.Vehicles;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Web.MapsRouting
+{
+    public static class MapsRoutingHelper
+    {
+        private const string _mapFileName = "latest-map.osm.pbf";
+        public static void InitializeMap(IKernel kernel)
+        {
+
+        }
+
+        private static void DownloadlMap()
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://download.geofabrik.de/europe/belarus-latest.osm.pbf", HttpContext.Current.Server.MapPath($"~/{_mapFileName}"));
+            }
+        }
+
+        private static void InitializeRouterDb()
+        {
+            var routerDb = new RouterDb();
+            using (var stream = new FileInfo(@"/path/to/some/osmfile.osm.pbf").OpenRead())
+            {
+                // create the network for cars only.
+                //routerDb.LoadOsmData(stream, Vehicle.Car);
+            }
+        }
+
+        private static void DeleteMap()
+        {
+
+        }
+    }
+}
