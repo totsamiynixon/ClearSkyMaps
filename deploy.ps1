14a4ba59a5d136e4291ebb4de4684c8cd1d9c4ed
yauhenibh <yauhenibh@gmail.com>
2018-10-03 01:25:37 +0300

    fix for appveyor.yml and deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -6,7 +6,7 @@
 [string]$accountName =  $env:APPVEYOR_ACCOUNT_NAME;
 [string]$projectName = $env:APPVEYOR_PROJECT_NAME;
 [string]$webSite = $env:WEB_SITE;
-[string]$stage = $env:DEPLOY_STAGE;
+[string]$stage = $env:ASPNETCORE_ENVIRONMENT;
 $postParams = @{AccountName=$accountName;ProjectName=$projectName;WebSite=$webSite;Stage=$stage;Token=$token;Package=$artifactPath}
 $Response = Invoke-WebRequest -Uri $web_depoy_host -Method POST -Body $postParams
 Write-Host $Response.Content
