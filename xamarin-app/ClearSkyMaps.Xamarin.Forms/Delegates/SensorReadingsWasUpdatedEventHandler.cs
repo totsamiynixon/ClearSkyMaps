3d0abfd276ce3d73b114af89a253a23cef6e72bb
yauhenibh <yauhenibh@gmail.com>
2018-11-21 01:25:36 +0300

    removed old project

--- xamarin-app/ClearSkyMaps.Xamarin.Forms/Delegates/SensorReadingsWasUpdatedEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using ClearSkyMaps.Xamarin.Forms.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ClearSkyMaps.Xamarin.Forms.Delegates
-{
-    public delegate void SensorReadingsWasUpdatedEventHandler(Reading newReading);
-}
