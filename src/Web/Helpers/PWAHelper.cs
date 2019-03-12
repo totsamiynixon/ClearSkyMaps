6fee4dca76b2390c72cf41ebb0008f18a0bb1df4
totsamiynixon <totsamiynixon@gmail.com>
2019-03-12 19:42:43 +0300

    fixed config.json visibility added pwa-helper (can be used in future)

---
+++ src/Web/Helpers/PWAHelper.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Web.Helpers
+{
+    public static class PWAHelper
+    {
+        public static void InitPWA()
+        {
+            InitManifestJson();
+            InitServiceWorkerJs();
+        }
+
+        private static void InitManifestJson()
+        {
+            var path = HostingEnvironment.MapPath(@"~/manifest.json");
+            var jsonString = File.ReadAllText(path);
+            var manifest = JsonConvert.DeserializeObject<JObject>(jsonString);
+            if (!SettingsHelper.IsProduction)
+            {
+                manifest["name"] = manifest["name"] + " " + SettingsHelper.ApplicationEnvironment;
+                manifest["short_name"] = manifest["short_name"] + " " + SettingsHelper.ApplicationEnvironment;
+            }
+            manifest["gsm_sender_id"] = SettingsHelper.FirebaseCloudMessagingMessagingSenderId;
+            File.WriteAllText(path, JsonConvert.SerializeObject(manifest));
+        }
+
+        private static void InitServiceWorkerJs()
+        {
+            var path = HostingEnvironment.MapPath(@"~/Scripts/front/service-worker.js");
+            var jsonString = File.ReadAllText(path);
+            var manifest = JsonConvert.DeserializeObject<JObject>(jsonString);
+            if (!SettingsHelper.IsProduction)
+            {
+                manifest["name"] = manifest["name"] + " " + SettingsHelper.ApplicationEnvironment;
+                manifest["short_name"] = manifest["short_name"] + " " + SettingsHelper.ApplicationEnvironment;
+            }
+            manifest["gsm_sender_id"] = SettingsHelper.FirebaseCloudMessagingMessagingSenderId;
+            File.WriteAllText(path, JsonConvert.SerializeObject(manifest));
+        }
+    }
+}
