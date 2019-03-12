1b8133357ee62cc64f9e75ce7fdb7a128e9f5de9
totsamiynixon <totsamiynixon@gmail.com>
2019-03-12 17:08:54 +0300

    fix

--- deploy/generate-config.ps1
+++ deploy/generate-config.ps1
@@ -1,9 +1,19 @@
-$jsonRequest = @{
-     "FirebaseCloudMessaging:ServerKey" = $env:FIREBASE_CLOUD_MESSAGING__SERVER_KEY;
-     "FirebaseCloudMessaging:MessagingSenderId" = $env:FIREBASE_CLOUD_MESSAGING__MESSAGING_SENDER_ID;
-     "ConnectionString" = $env:CONNECTION_STRING;
-     "Emulation:Enabled" = $env:EMULATION_ENABLED;
-     "Application:Environment" = $env:EMULATION_ENABLED;
-     "Application:Verison" = $env:APPLICATION_VERSION;
+param([Parameter(Mandatory=$true)][string]$path)
+try
+{
+    $jsonRequest = @{
+        "FirebaseCloudMessaging:ServerKey" = $env:FIREBASE_CLOUD_MESSAGING__SERVER_KEY;
+        "FirebaseCloudMessaging:MessagingSenderId" = $env:FIREBASE_CLOUD_MESSAGING__MESSAGING_SENDER_ID;
+        "ConnectionString" = $env:CONNECTION_STRING;
+        "Emulation:Enabled" = $env:EMULATION_ENABLED;
+        "Application:Environment" = $env:APPLICATION_ENVIRONMENT;
+        "Application:Verison" = $env:APPLICATION_VERSION;
+   }
+   $jsonRequest | ConvertTo-Json -depth 100 | Out-File $path;
 }
-$jsonRequest | ConvertTo-Json -depth 100 | Out-File "..\src\Web\release\config.json";
+catch
+{
+    Write-Host "$($_.Exception.Message)"  -ForegroundColor Red
+    Write-Host "$($_.InvocationInfo.PositionMessage)" -ForegroundColor Red
+    exit 1
+}
